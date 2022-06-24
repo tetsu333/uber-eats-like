b60b86b1c62e42d5bf85690af594d8afef9d290f
tetsu <cda07c5117@gmail.com>
2022-06-25 00:01:04 +0900

    注文モーダル実装

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -50,9 +50,11 @@ const ItemWrapper = styled.div`
   margin: 16px;
 `;
 
-export const Foods = ({
-  match
-}) => {
+const submitOrder = () => {
+  console.log('登録ボタンが押された！')
+}
+
+export const Foods=({match})=>{
   const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
 
   const initialState = {
@@ -117,14 +119,26 @@ export const Foods = ({
       </FoodsList>
       {
         state.isOpenOrderDialog &&
-          <FoodOrderDialog
-            food={state.selectedFood}
-            isOpen={state.isOpenOrderDialog}
-            onClose={() => setState({
-              ...state,
-              isOpenOrderDialog: false,
-            })}
-          />
+        <FoodOrderDialog
+          isOpen={state.isOpenOrderDialog}
+          food={state.selectedFood}
+          countNumber={state.selectedFoodCount}
+          onClickCountUp={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount + 1,
+          })}
+          onClickCountDown={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount - 1,
+          })}
+          onClickOrder={() => submitOrder()}
+          onClose={() => setState({
+            ...state,
+            isOpenOrderDialog: false,
+            selectedFood: null,
+            selectedFoodCount: 1,
+          })}
+        />
       }
     </Fragment>
   )
